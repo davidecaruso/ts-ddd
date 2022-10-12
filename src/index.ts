4872895e2591e38bd31fda141429f639cda3b073
Davide Caruso <davide.caruso93@gmail.com>
2022-10-12 14:53:22 +0200

    Add modules

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import * as application from './core/application'
+import * as domain from './core/domain'
+import * as infrastructure from './infrastructure'
+import * as presentation from './presentation'
+
+export {
+    application,
+    domain,
+    infrastructure,
+    presentation,
+}
